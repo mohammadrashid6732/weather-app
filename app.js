1a0bfd27df4d64a6addcf8465064633bd5dc4572
mohammadrashid <mohammadrashid6732@gmail.com>
2024-12-25 20:21:33 +0330

    write code for give current weather and forecast weather and show in the web and get weather with location

--- app.js
+++ app.js
@@ -1,1 +1,125 @@
-console.log('mohammad rashid')
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
+const API_KEY = "8a07319f66515d38204ea25ece2ab72b";
+const DAYS = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const searchInput = document.querySelector("input");
+const searchButton = document.querySelector("button");
+const weatherContainer = document.getElementById("weather");
+const forecastContainer = document.getElementById("forecast");
+const locationIcon = document.getElementById("location");
+
+const getCurrentWeatherByName = async (city) => {
+  const url = `${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric `;
+  const response = await fetch(url);
+  const json = response.json();
+  return json;
+};
+
+const getCurrentWeatherByCoordinates = async (lat, lon) => {
+  const url = `${BASE_URL}/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric `;
+  const response = await fetch(url);
+  const json = response.json();
+  return json;
+};
+
+const getForecastWeatherByName = async (city) => {
+  const url = `${BASE_URL}/forecast?q=${city}&appid=${API_KEY}&units=metric `;
+  const response = await fetch(url);
+  const json = response.json();
+  return json;
+};
+
+const getForecastWeatherCoordinates = async (lat, lon) => {
+  const url = `${BASE_URL}/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric `;
+  const response = await fetch(url);
+  const json = response.json();
+  return json;
+};
+
+const renderCurrentWeather = (data) => {
+  const weatherJSX = `
+      <h1>${data.name}, ${data.sys.country}</h1>
+    <div id="main">
+      <img alt="weather icons"
+        src="http://openweathermap.org/img/w/${data.weather[0].icon}.png"
+        
+      />
+      <span>${data.weather[0].main}</span>
+      <p>${Math.round(data.main.temp)} ℃</p>
+    </div>
+    <div id="info">
+      <p>Humidity: <span>${data.main.humidity}%</span></p>
+      <p>wind speed: <span>${data.wind.speed}m/s</span></p>
+    </div>
+    
+  `;
+  weatherContainer.innerHTML = weatherJSX;
+};
+
+const getWeekDay = (date) => {
+  return DAYS[new Date(date * 1000).getDay()];
+};
+
+const renderForecastWeather = (data) => {
+  data = data.list.filter((obj) => obj.dt_txt.endsWith("12:00:00"));
+  data.forEach((i) => {
+    const forecastJSX = `
+    <div>
+      <img
+        src="http://openweathermap.org/img/w/${i.weather[0].icon}.png"
+        alt="weather icon"
+      />
+      <h3>${getWeekDay(i.dt)}</h3>
+      <p>${Math.round(i.main.temp)}</p>
+      <span>${i.weather[0].main}</span>
+    </div>
+    `;
+    forecastContainer.innerHTML += forecastJSX;
+  });
+};
+
+const searchHandler = async () => {
+  const cityName = searchInput.value;
+  if (!cityName) {
+    alert("Please enter city name");
+  }
+  const currentData = await getCurrentWeatherByName(cityName);
+  renderCurrentWeather(currentData);
+  const forecastData = await getForecastWeatherByName(cityName);
+  renderForecastWeather(forecastData);
+};
+
+const positionCallback = async (position) => {
+  const { latitude, longitude } = position.coords;
+  const currentData = await getCurrentWeatherByCoordinates(latitude, longitude);
+  const foreCastData = await getForecastWeatherCoordinates(latitude, longitude);
+  renderCurrentWeather(currentData);
+  renderForecastWeather(foreCastData);
+}
+
+const errorCallback = (error) => {
+  console.log(error);
+};
+
+const locationHandler = () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(positionCallback, errorCallback);
+  } else {
+    alert("Your browser does not support geolocation");
+  }
+};
+
+searchButton.addEventListener("click", searchHandler);
+searchInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    searchHandler();
+  }
+});
+locationIcon.addEventListener("click", locationHandler);
